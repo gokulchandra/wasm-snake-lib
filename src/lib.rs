22f1dae6d757de849f8b521364734d6e0f24bbc7
gchandrasekaran <gokulchandra93@gmail.com>
2020-06-08 18:35:59 +1000

    [Gokul] implements a moving pointer

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::__rt::std::error::Error;
+use core::fmt;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -8,16 +10,37 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[derive(Debug, Clone)]
+struct OutOfBoundsError;
+
+impl fmt::Display for OutOfBoundsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Out of bounds")
+    }
+}
+
+#[wasm_bindgen]
+pub enum Direction {
+    Left = 0,
+    Right = 1,
+    Up = 2,
+    Down = 3,
+}
+
 #[wasm_bindgen]
 pub struct Map {
     height: u32,
     width: u32,
     cells: Vec<u32>,
+    game_over: bool,
 }
 
 impl Map {
-    fn get_index(&self, row: u32, column: u32) -> u32 {
-        return (row * self.width) + column;
+    fn get_index(&self, row: u32, column: u32) -> Result<u32, &str> {
+        if !(0..self.height).contains(&row) || !(0..self.width).contains(&column) {
+            return Result::Err("Out of bounds");
+        }
+        return Ok((row * self.width) + column);
     }
 }
 
@@ -40,7 +63,7 @@ impl Map {
         let width = 50;
         let mut cells: Vec<u32> = (0..height * width)
             .map(|i| -> u32 {
-                return if { ((height * width) / 2 + (width / 2)) == i } { 1 } else { 0 }
+                return if { ((height * width) / 2 + (width / 2)) == i } { 1 } else { 0 };
             })
             .collect();
 
@@ -48,6 +71,47 @@ impl Map {
             height,
             width,
             cells,
+            game_over: false,
         };
+    }
+
+    pub fn tick(&mut self, direction: Direction) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = match self.get_index(row, col) {
+                    Ok(idx) => idx as usize,
+                    Err(error) => {
+                        self.game_over = true;
+                        panic!("Game over!")
+                    }
+                };
+
+                let cell = self.cells[idx];
+                if cell == 0 { continue; }
+                next[idx] = 0;
+                match direction {
+                    Direction::Left => { self.handle_step(next.as_mut(), row, col - 1) }
+                    Direction::Right => { self.handle_step(next.as_mut(), row, col + 1) }
+                    Direction::Up => { self.handle_step(next.as_mut(), row - 1, col) }
+                    Direction::Down => { self.handle_step(next.as_mut(), row + 1, col) }
+                }
+            }
+        }
+
+        self.cells = next;
+    }
+
+    fn handle_step(&mut self, next_cells: &mut Vec<u32>, row: u32, column: u32) {
+        let idx = match self.get_index(row, column) {
+            Ok(idx) => idx as usize,
+            Err(error) => {
+                self.game_over = true;
+                panic!("Game over!")
+            }
+        };
+
+        next_cells[idx] = 1
     }
 }
